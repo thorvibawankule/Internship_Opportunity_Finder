45ec2656cace670c0660fc3c31cc865564887fe3
thorvibawankule <24bawankulet@rbunagpur.in>
2026-04-24 21:19:26 +0530

    Delete critic_agent.py

--- critic_agent.py
+++
@@ -1,9 +0,0 @@
-from crewai import Agent
-
-critic_agent = Agent(
-    role="Reviewer",
-    goal="Check email quality",
-    backstory="Gives feedback on email",
-    verbose=True,
-    allow_delegation=False
-)
